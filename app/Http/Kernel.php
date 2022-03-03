43effa3cac6a8c6f91d590d6f37c7cefa25c211c
Lexarque <ignishalcyon1@gmail.com>
2022-03-03 22:42:23 +0700

    Tidying things up 1.0

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
-        'jwt.auth' => Tymon\JWTAuth\Middleware\GetUserFromToken::class,
-        'jwt.refresh' => Tymon\JWTAuth\Middleware\RefreshToken::class,
     ];
 }
